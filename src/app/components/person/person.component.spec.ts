e81f9db175ac68141385b6ef90777e4068d663eb
kosmoSAR <santiagoaristizabal.kosmetikon@gmail.com>
2023-10-17 15:01:52 -0500

    comienzo clase de componentes

---
+++ src/app/components/person/person.component.spec.ts
@@ -0,0 +1,31 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PersonComponent } from './person.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+describe('PersonComponent', () => {
+  let component: PersonComponent;
+  let fixture: ComponentFixture<PersonComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PersonComponent]
+    });
+    fixture = TestBed.createComponent(PersonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have <p> with "Hola soy un parrafo', () => {
+    const personDebug: DebugElement = fixture.nativeElement;
+    // Usamos un lector de selectores de CSS de Angular
+    const pDebug: DebugElement = personDebug.query(By.css('p'));
+    const pElement: HTMLElement = pDebug.nativeElement;
+    expect(pElement?.textContent).toEqual('Hola soy un parrafo')
+  })
+});
