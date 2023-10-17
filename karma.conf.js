cffdf5af60f8eda273ac78158c58fdbae2ed30b1
kosmoSAR <santiagoaristizabal.kosmetikon@gmail.com>
2023-10-16 20:33:27 -0500

    config GithGub Actions

--- karma.conf.js
+++ karma.conf.js
@@ -43,6 +43,12 @@ module.exports = function (config) {
     },
     reporters: ['mocha'],
     browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     restartOnFileChange: true
   });
 };
